0bc856015a3b0155433f6ee839de42690df41f31
Your Name <ayesh.saleem2006@gmail.com>
2025-07-13 14:55:57 +0500

    Initial commit - E-commerce project

--- my-project/main.js
+++
@@ -1,9 +0,0 @@
-import './src/style.css';
-
-import products from "./api/products.json";
-import { showProductContainer } from './homeProductCards';
-
-
-console.log(products);
-// DEfine a function named 'showProductContainer' that take a arry of product input.
-showProductContainer(products);
